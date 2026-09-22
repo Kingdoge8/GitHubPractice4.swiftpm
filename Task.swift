0ace19090e2e9eece093af2d00fefc39356db09b
Kian Trotter <ktrotter8454@stu.d214.org>
2026-09-22 10:27:27 -0500

    added completed option in header view

--- Task.swift
+++ Task.swift
@@ -7,8 +7,7 @@
 
 import SwiftUI
 
-struct Task: Identifiable, Hashable {
-    var id = UUID()
+struct Task: Hashable {
     var name: String
     struct TaskView: View {
         @State var nameOfTask: String = ""
