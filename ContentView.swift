62d3681d4b53baae1adde8476a128453d6b70c80
Adam K <kingdodgemangay@gmail.com>
2026-09-22 10:28:01 -0500

    changed  tasks var and text

--- ContentView.swift
+++ ContentView.swift
@@ -3,10 +3,10 @@ import SwiftUI
 struct ContentView: View {
     @State var tasks: [Task] =  []
     var body: some View {
-        HeaderView()
+        HeaderView(tasks: Binding<[Task]>($tasks))
         List(tasks, id: \.self) { task in
-          
-            
+            Text("\($tasks.nameOfTask)")
+                .completed
         }
             
         
