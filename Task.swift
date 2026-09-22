f5ca73c942fbc24753f88fa4a54dbb90c4771f4c
Nolan <nvariano7417@stu.d214.org>
2026-09-22 07:58:45 -0700

    added bool var to task

--- Task.swift
+++ Task.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct Task: Identifiable, Hashable {
     var id = UUID()
     var name: String
+    var completed = false
     struct TaskView: View {
         @State var nameOfTask: String = ""
         var body: some View {
